bbf19540776ca34bded3c3ec470ee8f778eb617a
Yicong Fu <fuyicong1996@gmail.com>
2022-06-10 14:35:40 -0400

    add matcore2d and tests

--- examples/elasticity/make_mat_inlines.py
+++ examples/elasticity/make_mat_inlines.py
@@ -1,5 +1,5 @@
 
-def get_mat(s, mator='normal'):
+def get_mat_3d(s, mator='normal'):
     mat = []
     if mator == 'normal':
         for k in [
@@ -21,28 +21,52 @@ def get_mat(s, mator='normal'):
             mat.append('%s(%d, %d)'%(s, k[0], k[1]))
     return mat
 
-def get_subroutine(Aor='normal', Bor='normal', op='=', scale=False):
-    A = get_mat('A', Aor)
-    B = get_mat('B', Bor)
-    C = get_mat('C', 'normal')
+def get_mat_2d(s, mator='normal'):
+    mat = []
+    if mator == 'normal':
+        for k in [
+            (0, 0), (0, 1),
+            (1, 0), (1, 1)]:
+            mat.append('%s(%d, %d)'%(s, k[0], k[1]))
+    elif mator == 'transpose':
+        for k in [
+            (0, 0), (1, 0),
+            (0, 1), (1, 1)]:
+            mat.append('%s(%d, %d)'%(s, k[0], k[1]))
+    elif mator == 'symmetric':
+        for k in [
+            (0, 0), (0, 1),
+            (1, 0), (1, 1)]:
+            mat.append('%s(%d, %d)'%(s, k[0], k[1]))
+    return mat
+
+def get_subroutine(Aor='normal', Bor='normal', op='=', scale=False, ndim=3):
+    if ndim == 3:
+        A = get_mat_3d('A', Aor)
+        B = get_mat_3d('B', Bor)
+        C = get_mat_3d('C', 'normal')
+    else:
+        A = get_mat_2d('A', Aor)
+        B = get_mat_2d('B', Bor)
+        C = get_mat_2d('C', 'normal')
 
     product = []
-    for i in range(3):
-        for j in range(3):
+    for i in range(ndim):
+        for j in range(ndim):
             s = ''
-            for k in range(3):
-                s += '%s * %s'%(A[3*i + k], B[3*k + j])
-                if k < 2:
+            for k in range(ndim):
+                s += '%s * %s'%(A[ndim*i + k], B[ndim*k + j])
+                if k < ndim - 1:
                     s += ' + '
             product.append(s)
 
     name = ''
     if Aor == 'normal':
-        name = 'Mat3x3'
+        name = f'Mat{ndim}x{ndim}'
     elif Aor == 'transpose':
-        name = 'MatTrans3x3'
+        name = f'MatTrans{ndim}x{ndim}'
     elif Aor == 'symmetric':
-        name = 'Symm3x3'
+        name = f'Symm{ndim}x{ndim}'
 
     if Bor == 'normal':
         name += 'Mat'
@@ -71,7 +95,7 @@ def get_subroutine(Aor='normal', Bor='normal', op='=', scale=False):
         result = 'template<class AMatType, class BMatType, class CMatType>\n'
 
     result += 'inline void %s( %s ){\n'%(name, args)
-    for k in range(9):
+    for k in range(ndim * ndim):
         if scale:
             result += '  %s %s scale * (%s);\n'%(C[k], op, product[k])
         else:
@@ -103,6 +127,6 @@ kwargs_list = [
 s = ''
 for pair in or_pairs:
     for kwargs in kwargs_list:
-        s += get_subroutine(Aor=pair[0], Bor=pair[1], **kwargs)
+        s += get_subroutine(Aor=pair[0], Bor=pair[1], **kwargs, ndim=2)
 
 print(s)

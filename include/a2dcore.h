8a066df2ce382723d03028558b481068ef034486
gkennedy <graeme.kennedy@ae.gatech.edu>
2023-09-05 12:20:44 -0400

    added include file

---
+++ include/a2dcore.h
@@ -0,0 +1,28 @@
+#ifndef A2D_CORE_H
+#define A2D_CORE_H
+
+#include "a2dobjs.h"
+#include "ad/a2dstack.h"
+
+//
+
+#include "ad/a2dgemm.h"
+#include "ad/a2dgreenstrain.h"
+#include "ad/a2disotropic.h"
+#include "ad/a2dmat.h"
+#include "ad/a2dmatdet.h"
+#include "ad/a2dmatinv.h"
+#include "ad/a2dmatsum.h"
+#include "ad/a2dmattrace.h"
+#include "ad/a2dscalar.h"
+#include "ad/a2dscalarops.h"
+#include "ad/a2dsymmatmulttrace.h"
+#include "ad/a2dsymrk.h"
+#include "ad/a2dsymsum.h"
+#include "ad/a2dvec.h"
+#include "ad/a2dveccross.h"
+#include "ad/a2dvecnorm.h"
+#include "ad/a2dvecouter.h"
+#include "ad/a2dvecsum.h"
+
+#endif  //  A2D_CORE_H

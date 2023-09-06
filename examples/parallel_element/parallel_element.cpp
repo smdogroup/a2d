smdogroup/a2d

commit 9d78412bbc32bfab4628931718baf51b614d56be

moved constexpr arrays in ET to static variables of functions so that they can be accessed by CUDA device code

include/multiphysics/feelementtypes-inl.h
@@ -265,7 +265,8 @@ KOKKOS_FUNCTION int ElementTypes::get_quad_node(const int i, const int j) {
 template <index_t nx, index_t ny>
 KOKKOS_FUNCTION index_t ElementTypes::get_quad_bound_length(const index_t v0,
                                                             const index_t v1) {
-  if (get_quad_verts_cart(v0, 0) != get_quad_verts_cart(v1, 0)) {
+  auto QUAD_VERTS_CART = get_quad_verts_cart();
+  if (QUAD_VERTS_CART[v0][0] != QUAD_VERTS_CART[v1][0]) {
     return nx;
   } else {
     return ny;
@@ -290,9 +291,9 @@ template <index_t offset, index_t ndof, index_t nx, index_t ny, class ElemDof,
 KOKKOS_FUNCTION void ElementTypes::get_quad_vert_dof(index_t v,
                                                      const ElemDof& element,
                                                      EntityDof& entity) {
-  const index_t node =
-      get_quad_node<nx, ny>((nx - 1) * get_quad_verts_cart(v, 0),
-                            (ny - 1) * get_quad_verts_cart(v, 1));
+  auto QUAD_VERTS_CART = get_quad_verts_cart();
+  const index_t node = get_quad_node<nx, ny>((nx - 1) * QUAD_VERTS_CART[v][0],
+                                             (ny - 1) * QUAD_VERTS_CART[v][1]);
 
   const index_t start = offset + ndof * node;
   for (index_t i = 0; i < ndof; i++) {
@@ -318,9 +319,9 @@ template <index_t offset, index_t ndof, index_t nx, index_t ny, class EntityDof,
 KOKKOS_FUNCTION void ElementTypes::set_quad_vert_dof(index_t v,
                                                      const EntityDof& entity,
                                                      ElemDof& element) {
-  const index_t node =
-      get_quad_node<nx, ny>((nx - 1) * get_quad_verts_cart(v, 0),
-                            (ny - 1) * get_quad_verts_cart(v, 1));
+  auto QUAD_VERTS_CART = get_quad_verts_cart();
+  const index_t node = get_quad_node<nx, ny>((nx - 1) * QUAD_VERTS_CART[v][0],
+                                             (ny - 1) * QUAD_VERTS_CART[v][1]);
 
   const index_t start = offset + ndof * node;
   for (index_t i = 0; i < ndof; i++) {
@@ -347,22 +348,23 @@ template <index_t offset, bool ends, index_t ndof, index_t nx, index_t ny,
 KOKKOS_FUNCTION void ElementTypes::get_quad_bound_dof(const index_t b,
                                                       const ElemDof& element,
                                                       EntityDof& entity) {
+  auto QUAD_VERTS_CART = get_quad_verts_cart();
+
   // Get the first and last vertices on the edge
-  const index_t v0 = QUAD_BOUND_VERTS[b][0];
-  const index_t v1 = QUAD_BOUND_VERTS[b][1];
+  const index_t v0 = get_quad_bound_verts()[b][0];
+  const index_t v1 = get_quad_bound_verts()[b][1];
 
   // Get the starting index on the element
-  const index_t start =
-      get_quad_node<nx, ny>((nx - 1) * get_quad_verts_cart(v0, 0),
-                            (ny - 1) * get_quad_verts_cart(v0, 1));
+  const index_t start = get_quad_node<nx, ny>((nx - 1) * QUAD_VERTS_CART[0][0],
+                                              (ny - 1) * QUAD_VERTS_CART[0][1]);
 
   // Find the number of nodes along the u-edge
   const index_t nu = get_quad_bound_length<nx, ny>(v0, v1);
 
   // Get the increment
-  const int incr = get_quad_node<nx, ny>(
-      get_quad_verts_cart(v1, 0) - get_quad_verts_cart(v0, 0),
-      get_quad_verts_cart(v1, 1) - get_quad_verts_cart(v0, 1));
+  const int incr =
+      get_quad_node<nx, ny>(QUAD_VERTS_CART[1][0] - QUAD_VERTS_CART[0][0],
+                            QUAD_VERTS_CART[1][1] - QUAD_VERTS_CART[0][1]);
 
   if constexpr (ends) {
     index_t index = start;
@@ -411,22 +413,23 @@ KOKKOS_FUNCTION void ElementTypes::set_quad_bound_dof(const index_t b,
                                                       const index_t orient,
                                                       const EntityDof& entity,
                                                       ElemDof& element) {
+  auto QUAD_VERTS_CART = get_quad_verts_cart();
+
   // Get the first and last vertices on the edge
-  const index_t v0 = QUAD_BOUND_VERTS[b][orient];
-  const index_t v1 = QUAD_BOUND_VERTS[b][(orient + 1) % 2];
+  const index_t v0 = get_quad_bound_verts()[b][orient];
+  const index_t v1 = get_quad_bound_verts()[b][(orient + 1) % 2];
 
   // Get the starting index on the element
-  const index_t start =
-      get_quad_node<nx, ny>((nx - 1) * get_quad_verts_cart(v0, 0),
-                            (ny - 1) * get_quad_verts_cart(v0, 1));
+  const index_t start = get_quad_node<nx, ny>((nx - 1) * QUAD_VERTS_CART[0][0],
+                                              (ny - 1) * QUAD_VERTS_CART[0][1]);
 
   // Find the number of nodes along the u-edge
   const index_t nu = get_quad_bound_length<nx, ny>(v0, v1);
 
   // Get the increment
-  const int incr = get_quad_node<nx, ny>(
-      get_quad_verts_cart(v1, 0) - get_quad_verts_cart(v0, 0),
-      get_quad_verts_cart(v1, 1) - get_quad_verts_cart(v0, 1));
+  const int incr =
+      get_quad_node<nx, ny>(QUAD_VERTS_CART[1][0] - QUAD_VERTS_CART[0][0],
+                            QUAD_VERTS_CART[1][1] - QUAD_VERTS_CART[0][1]);
 
   if constexpr (ends) {
     index_t index = start;
@@ -576,6 +579,7 @@ KOKKOS_FUNCTION int ElementTypes::get_hex_node(const int i, const int j,
 template <index_t nx, index_t ny, index_t nz>
 KOKKOS_FUNCTION index_t ElementTypes::get_hex_edge_length(const index_t v0,
                                                           const index_t v1) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
   if (HEX_VERTS_CART[v0][0] != HEX_VERTS_CART[v1][0]) {
     return nx;
   } else if (HEX_VERTS_CART[v0][1] != HEX_VERTS_CART[v1][1]) {
@@ -604,6 +608,7 @@ template <index_t offset, index_t ndof, index_t nx, index_t ny, index_t nz,
 KOKKOS_FUNCTION void ElementTypes::get_hex_vert_dof(index_t v,
                                                     const ElemDof& element,
                                                     EntityDof& entity) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
   const index_t node = get_hex_node<nx, ny, nz>(
       (nx - 1) * HEX_VERTS_CART[v][0], (ny - 1) * HEX_VERTS_CART[v][1],
       (nz - 1) * HEX_VERTS_CART[v][2]);
@@ -633,6 +638,7 @@ template <index_t offset, index_t ndof, index_t nx, index_t ny, index_t nz,
 KOKKOS_FUNCTION void ElementTypes::set_hex_vert_dof(index_t v,
                                                     const EntityDof& entity,
                                                     ElemDof& element) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
   const index_t node = get_hex_node<nx, ny, nz>(
       (nx - 1) * HEX_VERTS_CART[v][0], (ny - 1) * HEX_VERTS_CART[v][1],
       (nz - 1) * HEX_VERTS_CART[v][2]);
@@ -663,9 +669,11 @@ template <index_t offset, bool ends, index_t ndof, index_t nx, index_t ny,
 KOKKOS_FUNCTION void ElementTypes::get_hex_edge_dof(const index_t e,
                                                     const ElemDof& element,
                                                     EntityDof& entity) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
+
   // Get the first and last vertices on the edge
-  const index_t v0 = HEX_EDGE_VERTS[e][0];
-  const index_t v1 = HEX_EDGE_VERTS[e][1];
+  const index_t v0 = get_hex_edge_verts()[e][0];
+  const index_t v1 = get_hex_edge_verts()[e][1];
 
   // Get the starting index on the element
   const index_t start = get_hex_node<nx, ny, nz>(
@@ -729,9 +737,11 @@ KOKKOS_FUNCTION void ElementTypes::set_hex_edge_dof(const index_t e,
                                                     const index_t orient,
                                                     const EntityDof& entity,
                                                     ElemDof& element) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
+
   // Get the first and last vertices on the edge
-  const index_t v0 = HEX_EDGE_VERTS[e][orient];
-  const index_t v1 = HEX_EDGE_VERTS[e][(orient + 1) % 2];
+  const index_t v0 = get_hex_edge_verts()[e][orient];
+  const index_t v1 = get_hex_edge_verts()[e][(orient + 1) % 2];
 
   // Get the starting index on the element
   const index_t start = get_hex_node<nx, ny, nz>(
@@ -790,10 +800,14 @@ template <index_t offset, bool ends, index_t ndof, index_t nx, index_t ny,
 KOKKOS_FUNCTION void ElementTypes::get_hex_bound_dof(const index_t b,
                                                      const ElemDof& element,
                                                      EntityDof& entity) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
+
   // Get the origin and bound vert directions
-  const index_t v0 = HEX_BOUND_VERTS[b][0];  // Root vertex
-  const index_t v1 = HEX_BOUND_VERTS[b][1];  // Vertex along the bound u dir
-  const index_t v3 = HEX_BOUND_VERTS[b][3];  // Vertex along the bound v dir
+  const index_t v0 = get_hex_bound_verts()[b][0];  // Root vertex
+  const index_t v1 =
+      get_hex_bound_verts()[b][1];  // Vertex along the bound u dir
+  const index_t v3 =
+      get_hex_bound_verts()[b][3];  // Vertex along the bound v dir
 
   // Get the root node location
   const index_t start = get_hex_node<nx, ny, nz>(
@@ -866,10 +880,14 @@ KOKKOS_FUNCTION void ElementTypes::set_hex_bound_dof(const index_t b,
                                                      const index_t orient,
                                                      const EntityDof& entity,
                                                      ElemDof& element) {
+  auto HEX_VERTS_CART = get_hex_verts_cart();
+
   // Get the origin and bound vert directions
-  const index_t v0 = HEX_BOUND_VERTS[b][0];  // Root vertex
-  const index_t v1 = HEX_BOUND_VERTS[b][1];  // Vertex along the bound u dir
-  const index_t v3 = HEX_BOUND_VERTS[b][3];  // Vertex along the bound v dir
+  const index_t v0 = get_hex_bound_verts()[b][0];  // Root vertex
+  const index_t v1 =
+      get_hex_bound_verts()[b][1];  // Vertex along the bound u dir
+  const index_t v3 =
+      get_hex_bound_verts()[b][3];  // Vertex along the bound v dir
 
   // Get the root node location
   const index_t start = get_hex_node<nx, ny, nz>(

examples/hdiv/hdiv.cpp
@@ -207,10 +207,11 @@ void set_geo_spherical(const T alpha, const T R, GeoElemVec& elem_geo) {
   T ulen = 2.0 / nx;
 
   T Xloc[3 * ET::HEX_NVERTS];
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (index_t ii = 0; ii < ET::HEX_NVERTS; ii++) {
-    T u = 1.0 * ET::HEX_VERTS_CART[ii][0];
-    T v = 1.0 * ET::HEX_VERTS_CART[ii][1];
-    T w = 1.0 * ET::HEX_VERTS_CART[ii][2];
+    T u = 1.0 * HEX_VERTS_CART[ii][0];
+    T v = 1.0 * HEX_VERTS_CART[ii][1];
+    T w = 1.0 * HEX_VERTS_CART[ii][2];
 
     Xloc[3 * ii] = (2.0 * u - 1.0) * a;
     Xloc[3 * ii + 1] = (2.0 * v - 1.0) * a;
@@ -247,6 +248,7 @@ void set_geo_spherical(const T alpha, const T R, GeoElemVec& elem_geo) {
   }
 
   // We know the 2-direction will be radial...
+  auto HEX_BOUND_VERTS = ET::get_hex_bound_verts();
   for (index_t face = 0; face < ET::HEX_NBOUNDS; face++) {
     for (index_t k = 0; k < nx; k++) {
       for (index_t j = 0; j < nx; j++) {
@@ -274,9 +276,9 @@ void set_geo_spherical(const T alpha, const T R, GeoElemVec& elem_geo) {
             T z0 = 0.0;
 
             for (index_t jj = 0; jj < 4; jj++) {
-              x0 += N[jj] * Xloc[3 * ET::HEX_BOUND_VERTS[face][jj]];
-              y0 += N[jj] * Xloc[3 * ET::HEX_BOUND_VERTS[face][jj] + 1];
-              z0 += N[jj] * Xloc[3 * ET::HEX_BOUND_VERTS[face][jj] + 2];
+              x0 += N[jj] * Xloc[3 * HEX_BOUND_VERTS[face][jj]];
+              y0 += N[jj] * Xloc[3 * HEX_BOUND_VERTS[face][jj] + 1];
+              z0 += N[jj] * Xloc[3 * HEX_BOUND_VERTS[face][jj] + 2];
             }
             T norm = std::sqrt(x0 * x0 + y0 * y0 + z0 * z0);
 
@@ -603,11 +605,12 @@ void find_spherical_error(bool write_sphere = false) {
   }
 
   // For each face add the faces
+  auto HEX_BOUND_VERTS = ET::get_hex_bound_verts();
   for (index_t face = 0; face < ET::HEX_NBOUNDS; face++) {
     for (index_t i = 0; i < 4; i++) {
-      hex[(face + 1) * ET::HEX_NVERTS + i] = hex[ET::HEX_BOUND_VERTS[face][i]];
+      hex[(face + 1) * ET::HEX_NVERTS + i] = hex[HEX_BOUND_VERTS[face][i]];
       hex[(face + 1) * ET::HEX_NVERTS + i + 4] =
-          hex_ext[ET::HEX_BOUND_VERTS[face][i]];
+          hex_ext[HEX_BOUND_VERTS[face][i]];
     }
   }
 

examples/sphere/sphere.cpp
@@ -24,10 +24,11 @@ void set_geo_spherical(const T alpha, const T R, GeoElemVec& elem_geo) {
   T ulen = 2.0 / nx;
 
   T Xloc[3 * ET::HEX_NVERTS];
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (index_t ii = 0; ii < ET::HEX_NVERTS; ii++) {
-    T u = 1.0 * ET::HEX_VERTS_CART[ii][0];
-    T v = 1.0 * ET::HEX_VERTS_CART[ii][1];
-    T w = 1.0 * ET::HEX_VERTS_CART[ii][2];
+    T u = 1.0 * HEX_VERTS_CART[ii][0];
+    T v = 1.0 * HEX_VERTS_CART[ii][1];
+    T w = 1.0 * HEX_VERTS_CART[ii][2];
 
     Xloc[3 * ii] = (2.0 * u - 1.0) * a;
     Xloc[3 * ii + 1] = (2.0 * v - 1.0) * a;
@@ -64,6 +65,7 @@ void set_geo_spherical(const T alpha, const T R, GeoElemVec& elem_geo) {
   }
 
   // We know the 2-direction will be radial...
+  auto HEX_BOUND_VERTS = ET::get_hex_bound_verts();
   for (index_t face = 0; face < ET::HEX_NBOUNDS; face++) {
     for (index_t k = 0; k < nx; k++) {
       for (index_t j = 0; j < nx; j++) {
@@ -91,9 +93,9 @@ void set_geo_spherical(const T alpha, const T R, GeoElemVec& elem_geo) {
             T z0 = 0.0;
 
             for (index_t jj = 0; jj < 4; jj++) {
-              x0 += N[jj] * Xloc[3 * ET::HEX_BOUND_VERTS[face][jj]];
-              y0 += N[jj] * Xloc[3 * ET::HEX_BOUND_VERTS[face][jj] + 1];
-              z0 += N[jj] * Xloc[3 * ET::HEX_BOUND_VERTS[face][jj] + 2];
+              x0 += N[jj] * Xloc[3 * HEX_BOUND_VERTS[face][jj]];
+              y0 += N[jj] * Xloc[3 * HEX_BOUND_VERTS[face][jj] + 1];
+              z0 += N[jj] * Xloc[3 * HEX_BOUND_VERTS[face][jj] + 2];
             }
             T norm = std::sqrt(x0 * x0 + y0 * y0 + z0 * z0);
 
@@ -546,11 +548,12 @@ void find_spherical_error(bool write_sphere = false) {
   }
 
   // For each face add the faces
+  auto HEX_BOUND_VERTS = ET::get_hex_bound_verts();
   for (index_t face = 0; face < ET::HEX_NBOUNDS; face++) {
     for (index_t i = 0; i < 4; i++) {
-      hex[(face + 1) * ET::HEX_NVERTS + i] = hex[ET::HEX_BOUND_VERTS[face][i]];
+      hex[(face + 1) * ET::HEX_NVERTS + i] = hex[HEX_BOUND_VERTS[face][i]];
       hex[(face + 1) * ET::HEX_NVERTS + i + 4] =
-          hex_ext[ET::HEX_BOUND_VERTS[face][i]];
+          hex_ext[HEX_BOUND_VERTS[face][i]];
     }
   }
 

examples/test_verify/verify_L2element.cpp
@@ -91,13 +91,14 @@ void main_body() {
   index_t *tets = NULL, *wedge = NULL, *pyrmd = NULL;
   index_t hex[8 * nhex];
 
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (index_t k = 0, e = 0; k < nz; k++) {
     for (index_t j = 0; j < ny; j++) {
       for (index_t i = 0; i < nx; i++, e++) {
         for (index_t ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          hex[8 * e + ii] = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                     j + ET::HEX_VERTS_CART[ii][1],
-                                     k + ET::HEX_VERTS_CART[ii][2]);
+          hex[8 * e + ii] =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
         }
       }
     }
@@ -173,9 +174,9 @@ void main_body() {
 
         // Get the geometry values
         for (index_t ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          index_t node = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                  j + ET::HEX_VERTS_CART[ii][1],
-                                  k + ET::HEX_VERTS_CART[ii][2]);
+          index_t node =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
 
           // Set the entity DOF
           index_t basis = 0;

examples/test_verify/verify_element.cpp
@@ -89,13 +89,14 @@ void main_body() {
   index_t *tets = NULL, *wedge = NULL, *pyrmd = NULL;
   index_t hex[8 * nhex];
 
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (index_t k = 0, e = 0; k < nz; k++) {
     for (index_t j = 0; j < ny; j++) {
       for (index_t i = 0; i < nx; i++, e++) {
         for (index_t ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          hex[8 * e + ii] = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                     j + ET::HEX_VERTS_CART[ii][1],
-                                     k + ET::HEX_VERTS_CART[ii][2]);
+          hex[8 * e + ii] =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
         }
       }
     }
@@ -167,9 +168,9 @@ void main_body() {
 
         // Get the geometry values
         for (index_t ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          index_t node = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                  j + ET::HEX_VERTS_CART[ii][1],
-                                  k + ET::HEX_VERTS_CART[ii][2]);
+          index_t node =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
 
           // Set the entity DOF
           index_t basis = 0;

examples/topology_optimization/topology_elasticity.cpp
@@ -176,13 +176,14 @@ create_analysis_box(std::string prefix, double vb_traction_frac, int b_nx,
 
   // Populate hex
   using ET = ElementTypes;
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (int k = 0, e = 0; k < b_nz; k++) {
     for (int j = 0; j < b_ny; j++) {
       for (int i = 0; i < b_nx; i++, e++) {
         for (int ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          hex[8 * e + ii] = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                     j + ET::HEX_VERTS_CART[ii][1],
-                                     k + ET::HEX_VERTS_CART[ii][2]);
+          hex[8 * e + ii] =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
         }
       }
     }

examples/topology_optimization/topology_heat.cpp
@@ -50,13 +50,14 @@ void main_body(std::string prefix, double bc_temp, double heat_source,
 
   // Populate hex
   using ET = A2D::ElementTypes;
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (int k = 0, e = 0; k < nz; k++) {
     for (int j = 0; j < ny; j++) {
       for (int i = 0; i < nx; i++, e++) {
         for (int ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          hex[8 * e + ii] = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                     j + ET::HEX_VERTS_CART[ii][1],
-                                     k + ET::HEX_VERTS_CART[ii][2]);
+          hex[8 * e + ii] =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
         }
       }
     }

examples/topology_optimization/toy_grad_check.cpp
@@ -32,14 +32,14 @@ int main(int argc, char *argv[]) {
   int hex[8 * nhex];
 
   using ET = A2D::ElementTypes;
-
+  auto HEX_VERTS_CART = ET::get_hex_verts_cart();
   for (int k = 0, e = 0; k < nz; k++) {
     for (int j = 0; j < ny; j++) {
       for (int i = 0; i < nx; i++, e++) {
         for (int ii = 0; ii < ET::HEX_NVERTS; ii++) {
-          hex[8 * e + ii] = node_num(i + ET::HEX_VERTS_CART[ii][0],
-                                     j + ET::HEX_VERTS_CART[ii][1],
-                                     k + ET::HEX_VERTS_CART[ii][2]);
+          hex[8 * e + ii] =
+              node_num(i + HEX_VERTS_CART[ii][0], j + HEX_VERTS_CART[ii][1],
+                       k + HEX_VERTS_CART[ii][2]);
         }
       }
     }

commit 5c344a529cd4aafd955a08f4ddf5c28d9a5a8447

fixed the bug introduced in 9d78412bbc32bfab4628931718baf51b614d56be

examples/parallel_element/parallel_element.cpp
@@ -150,7 +150,7 @@ int main(int argc, char *argv[]) {
   {
     using T = double;
 
-    constexpr bool smoke_test = true;
+    constexpr bool smoke_test = false;
 
     // Number of elements in each dimension
     const index_t degree = smoke_test ? 1 : 4;

include/multiphysics/feelementtypes-inl.h
@@ -355,16 +355,16 @@ KOKKOS_FUNCTION void ElementTypes::get_quad_bound_dof(const index_t b,
   const index_t v1 = get_quad_bound_verts()[b][1];
 
   // Get the starting index on the element
-  const index_t start = get_quad_node<nx, ny>((nx - 1) * QUAD_VERTS_CART[0][0],
-                                              (ny - 1) * QUAD_VERTS_CART[0][1]);
+  const index_t start = get_quad_node<nx, ny>(
+      (nx - 1) * QUAD_VERTS_CART[v0][0], (ny - 1) * QUAD_VERTS_CART[v0][1]);
 
   // Find the number of nodes along the u-edge
   const index_t nu = get_quad_bound_length<nx, ny>(v0, v1);
 
   // Get the increment
   const int incr =
-      get_quad_node<nx, ny>(QUAD_VERTS_CART[1][0] - QUAD_VERTS_CART[0][0],
-                            QUAD_VERTS_CART[1][1] - QUAD_VERTS_CART[0][1]);
+      get_quad_node<nx, ny>(QUAD_VERTS_CART[v1][0] - QUAD_VERTS_CART[v0][0],
+                            QUAD_VERTS_CART[v1][1] - QUAD_VERTS_CART[v0][1]);
 
   if constexpr (ends) {
     index_t index = start;
@@ -420,16 +420,16 @@ KOKKOS_FUNCTION void ElementTypes::set_quad_bound_dof(const index_t b,
   const index_t v1 = get_quad_bound_verts()[b][(orient + 1) % 2];
 
   // Get the starting index on the element
-  const index_t start = get_quad_node<nx, ny>((nx - 1) * QUAD_VERTS_CART[0][0],
-                                              (ny - 1) * QUAD_VERTS_CART[0][1]);
+  const index_t start = get_quad_node<nx, ny>(
+      (nx - 1) * QUAD_VERTS_CART[v0][0], (ny - 1) * QUAD_VERTS_CART[v0][1]);
 
   // Find the number of nodes along the u-edge
   const index_t nu = get_quad_bound_length<nx, ny>(v0, v1);
 
   // Get the increment
   const int incr =
-      get_quad_node<nx, ny>(QUAD_VERTS_CART[1][0] - QUAD_VERTS_CART[0][0],
-                            QUAD_VERTS_CART[1][1] - QUAD_VERTS_CART[0][1]);
+      get_quad_node<nx, ny>(QUAD_VERTS_CART[v1][0] - QUAD_VERTS_CART[v0][0],
+                            QUAD_VERTS_CART[v1][1] - QUAD_VERTS_CART[v0][1]);
 
   if constexpr (ends) {
     index_t index = start;